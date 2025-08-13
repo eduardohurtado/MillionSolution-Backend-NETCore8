3e1723002342314b3a1909bcf34ecb552df268c1
Eduardo Hurtado <lehurtadog@outlook.com>
2025-08-12 22:01:09 -0500

    Feature(003): - Added docker engine to deploy .NETCore 8 app and MongoDB instance. - Added Docker config file. - Code fixes.

--- Million.Api/Dtos/PropertyDto.cs
+++ Million.Api/Dtos/PropertyDto.cs
@@ -1,5 +1,10 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
 public class PropertyDto
 {
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
     public string? Id { get; set; }
     public string IdOwner { get; set; } = null!;
     public string Name { get; set; } = null!;
--- Million.Api/Program.cs
+++ Million.Api/Program.cs
@@ -16,7 +16,6 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -28,36 +27,10 @@ app.MapControllers();
 app.UseHttpsRedirection();
 
 // run migrations at startup
-using(var scope = app.Services.CreateScope())
+using (var scope = app.Services.CreateScope())
 {
     var migrator = new MongoMigrations(scope.ServiceProvider.GetRequiredService<MongoContext>());
     await migrator.RunMigrationsAsync();
 }
 
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
